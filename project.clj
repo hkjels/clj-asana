def92492e33add69bc9e872b576c2505408204dc
Akash Kothawale <akash@decached.com>
2013-09-16 21:40:59 +0530

    Bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clj-asana "0.0.1-SNAPSHOT"
+(defproject clj-asana "0.1.0"
   :description "Clojure Wrapper for Asana API."
   :url "https://github.com/decached/clj-asana"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.6"]]
-  :main clj-asana.core)
+                 [clj-http "0.7.6"]])
--- test/clj_asana/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-asana.core-test
-  (:require [clojure.test :refer :all]
-            [clj-asana.core :refer :all]))
-
-(deftest a-test
-  (testing "DON'T FIXME, I pass."
-    (is (= 1 1))))
